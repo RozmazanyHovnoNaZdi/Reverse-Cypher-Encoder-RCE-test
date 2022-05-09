7d148b41b8bc02cfad495459a8d538bf58ad0ed7
Jakub Matějka <jakub.matejka@educhem.cz>
2022-05-09 13:24:45 +0200

    Přidejte soubory projektu.

---
+++ davidmávelkýpéro/MainWindow.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace davidmávelkýpéro
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        string znaky = "aábcčdďeéěfghiíjklmnňoópqrřsštťuúůvwxyýzž,.; ";
+        string znakyObracene = "žzýyxwvůúuťtšsřrqpóoňnmlkjíihgfěéeďdčcbáa,.; ";
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Encode_Click(object sender, RoutedEventArgs e)
+        {
+            EncodeOutro.Text = EncoderMetoda();
+        }
+        private string EncoderMetoda()
+        {
+            string obsahTxt = EncodeEntry.Text.ToLower();
+            string prevracenyTxt = "";
+            for (int i = 0; i < obsahTxt.Length; i++)
+            {
+                char znak = obsahTxt[i];
+                int index = znaky.IndexOf(znak);
+                prevracenyTxt += znakyObracene[index];
+            }
+            return prevracenyTxt;
+        }
+    }
+}
